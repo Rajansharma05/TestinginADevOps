f3fd7c9b8c12bd44591d971a3ba5f3e640b23530
Rajansharma05 <rajan.sharma_cs18@gla.ac.in>
2023-09-05 15:39:57 +0530

    initial

--- DemoTest/src/main/java/com/example/NewTest2.java
+++ DemoTest/src/main/java/com/example/NewTest2.java
@@ -1,4 +1,4 @@
-
+/**
 /**
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -99,7 +99,7 @@ public class NewTest2 {
         };
     }
 }**/
-
+/**
 package com.example;
 
 import org.openqa.selenium.By;
@@ -153,4 +153,123 @@ public class NewTest2 {
     }
 }
 
+**/
+
+package com.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.Test;
+
+public class NewTest2 {
+    private WebDriver driver;
+
+    @BeforeGroups(groups = {"Shopping"})
+    public void beforeShoppingGroup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\RAJAN\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        System.out.println("Redirecting to Amazon.in");
+        driver.get("https://www.amazon.in");
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test(groups = {"Shopping"})
+    public void test1() {
+        // Registration (Sign up) on Amazon
+        System.out.println("Test 1 for Shopping - Registration");
+
+        // Navigate to Amazon's registration page (You may need to adapt this URL)
+        driver.get("https://www.amazon.in/ap/register");
+
+        // Fill in registration form fields
+        WebElement nameField = driver.findElement(By.id("ap_customer_name"));
+        nameField.sendKeys("John Doe");
+
+        WebElement emailField = driver.findElement(By.id("ap_email"));
+        emailField.sendKeys("johndoe@example.com");
+
+        WebElement passwordField = driver.findElement(By.id("ap_password"));
+        passwordField.sendKeys("yourpassword");
+
+        WebElement reenterPasswordField = driver.findElement(By.id("ap_password_check"));
+        reenterPasswordField.sendKeys("yourpassword");
+
+        WebElement continueButton = driver.findElement(By.id("continue"));
+        continueButton.click();
+
+        // Additional registration steps can be added as needed
+
+        // Verify successful registration
+        WebElement successMessage = driver.findElement(By.xpath("//div[@class='a-box-inner']/h4"));
+        assert successMessage.getText().contains("Hello, John Doe");
+    }
+
+    @Test(groups = {"Shopping"})
+    public void test2() {
+        // Login to Amazon
+        System.out.println("Test 2 for Shopping - Login");
+
+        // Navigate to Amazon's login page (You may need to adapt this URL)
+        driver.get("https://www.amazon.in/ap/signin");
+
+        // Fill in login credentials
+        WebElement emailField = driver.findElement(By.id("ap_email"));
+        emailField.sendKeys("johndoe@example.com");
+
+        WebElement passwordField = driver.findElement(By.id("ap_password"));
+        passwordField.sendKeys("yourpassword");
+
+        WebElement signInButton = driver.findElement(By.id("signInSubmit"));
+        signInButton.click();
+
+        // Additional login steps can be added as needed
+
+        // Verify successful login (You can add assertions or checks here)
+    }
+
+    @Test(groups = {"Shopping"})
+    public void test3() {
+        // Search on Amazon
+        System.out.println("Test 3 for Shopping - Search");
+
+        // Locate the search input field and enter a search query
+        WebElement searchInput = driver.findElement(By.id("twotabsearchtextbox"));
+        searchInput.sendKeys("laptop");
+
+        // Submit the search query
+        searchInput.submit();
+
+        // Additional search-related actions can be added as needed
+
+        // Verify search results (You can add assertions or checks here)
+    }
+
+    @Test(groups = {"Shopping"})
+    public void test4() {
+        // Add-to-Cart on Amazon
+        System.out.println("Test 4 for Shopping - Add to Cart");
+
+        // Locate and click on a product (You may need to adapt this selector)
+        WebElement productLink = driver.findElement(By.xpath("//a[contains(@href, '/product/')]"));
+        productLink.click();
+
+        // Locate and click on the "Add to Cart" button (You may need to adapt this selector)
+        WebElement addToCartButton = driver.findElement(By.id("add-to-cart-button"));
+        addToCartButton.click();
+
+        // Additional add-to-cart actions can be added as needed
+
+        // Verify that the product has been added to the cart (You can add assertions or checks here)
+    }
+}
 
