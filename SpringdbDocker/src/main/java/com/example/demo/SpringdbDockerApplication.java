4e6b0e3a54c91543e8cbee9517f0e3a40e7347eb
Rajansharma05 <rajan.sharma_cs18@gla.ac.in>
2023-09-07 16:30:25 +0530

    initial commit

---
+++ SpringdbDocker/src/main/java/com/example/demo/SpringdbDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdbDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdbDockerApplication.class, args);
+	}
+
+}
